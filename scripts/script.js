148341d68d5ec42fb979e7a44590ce1d2ad73c1c
Dominik <dominik.karas@clurgo.com>
2024-08-26 14:37:12 +0200

    Add closing of dropdown menu after clicking some option

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const dropdown = document.querySelector('.dropdown');
     const crossSign = document.querySelector('.cross-sign');
+    const navLinks = document.querySelectorAll('.dropdown .nav-links li');
 
     hamburger.addEventListener('click', () => {
         dropdown.classList.toggle('open');
     });
 
     crossSign.addEventListener('click', () => {
-        dropdown.classList.toggle('open');
+        dropdown.classList.remove('open');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            setTimeout(() => {
+                dropdown.classList.remove('open');
+            }, 500);
+        });
     });
 });
